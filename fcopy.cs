a9d5bb57175e5db16e4adf6e59bb963257e424ad
Kody Brown <kody.brown@venafi.com>
2012-08-09 18:58:53 -0600

    - Updated the formatting/copyright header

--- fcopy.cs
+++ fcopy.cs
@@ -1,32 +1,14 @@
-///////////////////////////////////////////////////////////////////////////////
 //
-//	Copyright (C) 2003-2006 Bricksoft.com
+// Copyright (C) 2003-2006 Bricksoft.com.
+// All Rights Reserved.
 //
-//	Bricksoft.com Developer PowerCode
+// Bricksoft.com PowerTools
 //
-//	This source code contains information, which is the proprietary property of
-//	Bricksoft.com.  This source code is received in confidence and its
-//	contents cannot be disclosed or copied without the prior written consent of
-//	Bricksoft.com.
+// This program is unpublished proprietary source code of Bricksoft.com.
+// Your use of this code is limited to those rights granted in the license between you and Bricksoft.com.
 //
-//	Nothing in this source code constitutes a guaranty, warranty, or license,
-//	express or implied. Bricksoft.com disclaims all liability for all
-//	such guaranties, warranties, and licenses, including but not limited to: 
-//	Fitness for a particular purpose; merchantability; not infringement of
-//	intellectual property or other rights of any third party or of
-//	Bricksoft.com; indemnity; and all others.  The reader is advised
-//	that third parties can have intellectual property rights that can be
-//	relevant to this source code and the technologies discussed herein, and is
-//	advised to seek the advice of competent legal counsel, without obligation
-//	of Bricksoft.com.
+// Author: Kody Brown (kody@bricksoft.com)
 //
-//	Bricksoft.com retains the right to make changes to this source
-//	code at any time, without notice.  Bricksoft.com makes no warranty
-//	for the use of this source code and assumes no responsibility for any
-//	errors that can appear in the source code nor does it make a commitment to
-//	update the information contained herein.
-//
-///////////////////////////////////////////////////////////////////////////////
 
 using System;
 using System.Diagnostics;
